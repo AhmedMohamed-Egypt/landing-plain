0e5d58915c3744c0632cfb6e37df1370721e4190
Ahmed-Mohamed-Egypt <ahmedatyabas@gmail.com>
2024-05-24 21:33:48 +0300

    update

---
+++ js/script.js
@@ -0,0 +1,68 @@
+function translateElemnts(){
+    const allImgs = document.querySelectorAll(".herocontent__leftside--managers--imgs img")
+    allImgs.forEach((item,index)=>{
+        if(index !==0 ) {
+            item.style.transform=(`translateX(${-10*(index)}px)`)
+        }
+    })
+}
+translateElemnts()
+
+const swiper = new Swiper(".testmonial__swiper .swiper", {
+    // Default parameters
+    slidesPerView: 3,
+    spaceBetween: 5,
+    speed: 500,
+  
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+      },
+      // when window width is >= 480px
+      767: {
+        slidesPerView: 2,
+      },
+      // when window width is >= 640px
+      994: {
+        slidesPerView: 3,
+        
+      },
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+  //scrollnavbar
+
+function scrollingNavBar(scrolling) {
+  
+
+  if (scrolling > 50) {
+    document.body.classList.add("fixed");
+  } else {
+    document.body.classList.remove("fixed");
+  }
+}
+window.onscroll =()=>{
+  scrollingNavBar(window.scrollY)
+}
+
+function clickHamburg(){
+  const hamBurgBtn = document.querySelector(".habmurgMenu")
+  let show = false
+  hamBurgBtn.addEventListener('click',()=>{
+    if(!show){
+      document.body.classList.add('sliding')
+      show = true
+    }else {
+      document.body.classList.remove('sliding')
+      show = false
+    }
+  })
+}
+clickHamburg()
